14348af2d80cbd926e5a667b936575c9ead9ca32
Eljacobz <89886318+Eljacobz@users.noreply.github.com>
2024-02-12 22:04:29 +0700

    Login Done

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -14,7 +14,7 @@ describe('Login Functionality', () => {
     });
     it('Verify failed login - email valid - password invalid', () => {
         cy.login('jacob@gmail.com','jacob221');
-        cy.get('.message-error > dic').should('be.visible');
+        cy.get('.message-error > div').should('be.visible');
         cy.wait(500)
     });
     it('Verify failed login - email invalid', () => {
@@ -22,6 +22,14 @@ describe('Login Functionality', () => {
         cy.get('.message-error > div').should('be.visible');
         cy.wait(500)
     });
-    
-    
+    it('Verify Success login and go to account detail', () => {
+        cy.login('jacob@gmail.com','Jacob123');
+        Navigation.visitAccount();
+        cy.get('.base').contains("My Account").should('be.visible');
+        cy.wait(500);
+        cy.get('.current > strong').contains('My Account').click();
+        cy.get('.block-dashboard-info > .block-title > strong').contains('Account Information').should('be.visible');
+        cy.wait(500)
+    }); 
+   
 });
--- cypress/support/pageObject/Navigation.js
+++ cypress/support/pageObject/Navigation.js
@@ -1,6 +1,9 @@
 class Navigation{
-    isitHomepage() {
+    visitHomepage() {
         cy.visit("https://magento.softwaretestingboard.com");
+      }
+      visitAccount() {
+        cy.visit("https://magento.softwaretestingboard.com/customer/account/");
       }
 }
 export default new Navigation();
